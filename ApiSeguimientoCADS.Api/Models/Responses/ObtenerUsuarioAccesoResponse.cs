345cb83e9e01a0efe4a7fcd7376997177c5d4605
Jorge Rutherford Krefft <rutherford.jorge@gmail.com>
2025-10-20 12:50:58 -0300

    elimina ProductModel y referencias

--- ApiSeguimientoCADS.Api/Models/Responses/ObtenerUsuarioAccesoResponse.cs
+++ ApiSeguimientoCADS.Api/Models/Responses/ObtenerUsuarioAccesoResponse.cs
@@ -5,9 +5,6 @@
 namespace ApiSeguimientoCADS.Api.Models.Responses
 {
     using ApiSeguimientoCADS.Api.Models.Headers;
-    using ApiSeguimientoCADS.Api.Services.Models;
-    using System;
-    using System.Collections.Generic;
 
     /// <summary>
     /// Respuesta para exponer el catálogo de productos.
@@ -18,10 +15,5 @@ namespace ApiSeguimientoCADS.Api.Models.Responses
         /// Obtiene o establece la cabecera estándar de la respuesta.
         /// </summary>
         public HeaderBase Header { get; set; } = new();
-
-        /// <summary>
-        /// Obtiene o establece la colección de productos.
-        /// </summary>
-        public IReadOnlyCollection<ProductModel> Items { get; set; } = Array.Empty<ProductModel>();
     }
 }
